5562520842b51c02c737a762029a81327c97919d
Curtis Fenner <cwfenner@umich.edu>
2018-04-30 21:50:49 -0400

    Remove setup.bat

--- setup.bat
+++
@@ -1,6 +0,0 @@
-echo|set /p= folder=$1;> smolc.sh
-echo|set /p= shift;>> smolc.sh
-echo|set /p= lua %cd:\=/%/src/compiler.lua --sources $folder/*.smol --main $*>> smolc.sh
-
-echo @echo off > smolc.bat
-echo sh %cd%/smolc.sh %%* >> smolc.bat
